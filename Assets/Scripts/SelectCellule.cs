ab5a46b9c60e06c09dad79b95eaf6823ae374d3b
Meli83 <meli83@orange.fr>
2021-06-01 16:00:04 +0200

    Modif de SelectCellule

--- Assets/Scripts/SelectCellule.cs
+++ Assets/Scripts/SelectCellule.cs
@@ -3,17 +3,24 @@ using System.Collections;
 
 public class SelectCellule : MonoBehaviour {
     public Camera camera;
-
+    public Transform Cellfav;
+    
 void Update(){
-   if (Input.GetMouseButtonDown(0)){ // if left button pressed...
+   if (Input.GetMouseButtonDown(0)){ // if left button pressed... 1 = right button and 2 = scroll button
         RaycastHit hit;
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         
         if (Physics.Raycast(ray, out hit)) {
-            Transform objectHit = hit.transform;
-            Debug.Log(objectHit.transform.name);
-            // Do something with the object that was hit by the raycast.
+            Cellfav = hit.transform;
+            //Surbrillance de l'objet
+
+            //Affichage de la description de la cellule
+            print("Coucou on m'a cliqué dessus");
+            foreach (var kvp in Cellfav.GetComponent<Metadata>().cell_data)
+    	        Debug.Log(kvp.Key + " : " + kvp.Value);
         }
     }
+    
 }
+
 }
