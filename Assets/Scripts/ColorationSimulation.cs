41457632b0986ccd148a96abe4265d05f0a0ced1
Meli83 <meli83@orange.fr>
2021-06-17 17:00:36 +0200

    suppression de mon interface et fonction MinMaxRelatif qui fonctionne (modif des données cell_data)

--- Assets/Scripts/ColorationSimulation.cs
+++ Assets/Scripts/ColorationSimulation.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Globalization;
+using System.Linq;
 
-public class Colorationsimulation : MonoBehaviour
+public class ColorationSimulation : MonoBehaviour
 {
 
     public double MaxAbsolu;
     public double MinAbsolu;
-    public bool IsAbsolu = false; //true si mode coloration absolu false sinon 
+    public bool IsAbsolu; //true si mode coloration absolu false sinon 
     public string Propriete;
 
     /*
@@ -18,34 +19,56 @@ public class Colorationsimulation : MonoBehaviour
         Sortie : un tableau de double MinMax contenant le minimum et le maximum des valeurs des cellules pour la propriete Prop
         MinMaxRelatif 
         MinMaxRelatif permettra de donner le min et le max suivant un pas de temps donne
+        Il faut vérifier si dans les donnees qu'on utilise, il n'y a pas de "" dans le nom des proprietes
     */
     public double[] MinMaxRelatif(string Prop){
         double[] MinMax = new double[2];
 
-        var dataset = Resources.Load<TextAsset>("Cells_data");
-        var dataLines = dataset.text.Split('\n');
-        foreach (var firstcell in GameObject.Find("1").GetComponent<Metadata>().cell_data){
-    	    if(firstcell.Key.Equals(Prop)){
-                MinMax[0] = Convert.ToDouble(firstcell.Value); //Emplacement du min
-                MinMax[1] = Convert.ToDouble(firstcell.Value); //Emplacement du max
-            }
-        }
-        for(int i = 2; i < dataLines.Length; i++) {
-            foreach (var cell in GameObject.Find(i.ToString()).GetComponent<Metadata>().cell_data){
-                if(cell.Key.Equals(Prop)){
-                    if(Convert.ToDouble(cell.Value) <= MinMax[0]){
-                        MinMax[0] = Convert.ToDouble(cell.Value);
-                    }
-                    if(Convert.ToDouble(cell.Value) > MinMax[1]){
-                        MinMax[1] = Convert.ToDouble(cell.Value);
-                    }
+        //Verification du nombre d endant pour ne pas renvoyer d erreurs
+        if(GameObject.Find("Tissue").transform.childCount > 0){
+
+            GameObject cell = GameObject.Find("Tissue").transform.GetChild(0).gameObject;
+
+            Metadata cell_meta = cell.GetComponent<Metadata>();
+            Dictionary<string, string> dict = cell_meta.cell_data;
+                
+            //Initialisation du tableau
+            MinMax[0] =  double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture); //Emplacement du min
+            MinMax[1] =  double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture); //Emplacement du min
+
+            //truc
+            //GameObject cell;
+            //Metadata cell_meta;
+            //boucle for qui va iterer sur le nombre d enfants
+            for(int i = 1; i < GameObject.Find("Tissue").transform.childCount; i = i + 1 ){
+                cell = GameObject.Find("Tissue").transform.GetChild(i).gameObject;
+                cell_meta = cell.GetComponent<Metadata>();
+                dict = cell_meta.cell_data;
+                if(double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture) <= MinMax[0]){
+                    MinMax[0] = double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture);
+                }
+                if(double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture) > MinMax[1]){
+                    MinMax[1] = double.Parse(dict[Prop], System.Globalization.CultureInfo.InvariantCulture);
                 }
             }
+
+
+            return MinMax;
         }
-        return MinMax;
+        else{
+            MinMax[0] = 0;
+            MinMax[1] = 0;
+            return MinMax;
+        }
     }
 
-    /*public void ColorationCellule(double min, double max, float valeur){
+    /*
+    Specification:
+        Entree : Deux doubles min et max et un string valeur
+        Cette fonction doit colorer la cellule en fonction de son string valeur et pour ça elle va utiliser une fonction
+        d'interpolation en prenant les doubles min et max comme valeur minimale et maximale.
+    */
+    /*public void ColorationCellule(double min, double max, string valeur){
         
 
     }*/
@@ -61,15 +84,16 @@ public class Colorationsimulation : MonoBehaviour
         if(IsAbsolu){
             double Min = MinAbsolu;
             double Max = MaxAbsolu;
+            print("Min = "+Min + " Max ="+ Max);
         }
         else{
             double[] MinMaxR = MinMaxRelatif(Propriete);
             double Min = MinMaxR[0];
             double Max = MinMaxR[1];
-            print("Min = "+Min + "Max ="+ Max);
+            print("Min = "+Min + " Max ="+ Max);
         }
         /*
-        Ici création d'une boucle 
+        Ici création d'une boucle utilisant la fonction ColorationCellule et (Min, Max)
 
         */
     }
